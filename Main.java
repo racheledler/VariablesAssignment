0acbdb5e1b06aa55ff717dc0e0f79116fd71ea5a
racheledler <55890230+racheledler@users.noreply.github.com>
2020-08-31 16:40:27 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main (String[] args)
+    {
+int testscore = 50;
+float price = 24.99f;
+String name = "Bobby Johnson";
+
+System.out.println("Test Score initialized is: " + testscore);
+System.out.println("Price tag: " + price);
+System.out.println("Username: " + name);
+
+
+    }
+
+}
